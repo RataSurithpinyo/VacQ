c2f8d333e262c32c63fd62fd42036b8b33f33118
rata <rataprim30026@gmail.com>
2023-02-08 22:34:01 +0700

    connect with MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     mongoose.set('strictQuery', true);
-    const conn = await mongoose.connect(process.env.MONGO_URI,{
-        userNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
 }
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const connectDB = require('./config/db');
 dotenv.config({path: './config/config.env' });
 
 connectDB();
-
 const app = express();
+app.use(express.json());
 const hospitals = require('./routes/hospitals');
 app.use('/api/v1/hospitals',hospitals);
 
-const PORT = process.env.PORT || 3000 ;
-app.listen(PORT,console.log('Server running in',process.env.NODE_ENV,'mode on port',PORT));
+const PORT = process.env.PORT ;
+//app.listen(PORT,console.log('Server running in',process.env.NODE_ENV,'mode on port',PORT));
 
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
 
